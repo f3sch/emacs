65f12dc6a561e9d29982ebfaddf020639f0de620
Felix Schlepper <f3sch.git@outlook.com>
2023-05-08 11:13:31 +0200

    style(indent): fixes
    Signed-off-by: Felix Schlepper <f3sch.git@outlook.com>

--- test-startup.sh
+++ test-startup.sh
@@ -3,23 +3,23 @@
 
 echo "Checking parens..."
 ${EMACS:=emacs} -nw --batch -Q \
-		        --eval "(progn
-                        (find-file \"init.el\")
-                        (check-parens))"
+                --eval "(progn
+                         (find-file \"init.el\")
+                         (check-parens))"
 echo "No formatting issues"
 
 echo "Attempting startup..."
 ${EMACS:=emacs} -nw --batch \
                 --eval '(progn
-                        (defvar url-show-status)
-                        (let ((debug-on-error t)
-                              (url-show-status nil)
-                              (user-emacs-directory default-directory)
-                              (user-init-file (expand-file-name "init.el"))
-                              (load-path (delq default-directory load-path)))
-                           (load-file user-init-file)
-                           (run-hooks (quote after-init-hook))
-			   (all-the-icons-install-fonts t)
-			   (nerd-icons-install-fonts t)))'
+                         (defvar url-show-status)
+                         (let ((debug-on-error t)
+                               (url-show-status nil)
+                               (user-emacs-directory default-directory)
+                               (user-init-file (expand-file-name "init.el"))
+                               (load-path (delq default-directory load-path)))
+                            (load-file user-init-file)
+                            (run-hooks (quote after-init-hook))
+                            (all-the-icons-install-fonts t)
+                            (nerd-icons-install-fonts t)))'
 
 echo "Startup successful"
